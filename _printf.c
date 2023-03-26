3499a1b84c86def82a1af60e89da18694a2c1761
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-03-26 23:28:26 +0300

    rename 0-print.c file to _printf.c

--- _printf.c
+++ _printf.c
@@ -1,49 +1,94 @@
 #include "main.h"
 /**
- * _printf - prints a name
- * _putchar - prints a char c
- * @format: string containing the name to print
- * Return: printed characters
+ * print_char - prints a single character to the standard output
+ * @arg: A va_list containing the character to print
+ * Return: The number of characters printed (always 1)
  */
-int _printf(const char *format, ...)
+int print_char(va_list arg)
 {
-int count = 0;
-va_list args;
-va_start(args, format);
-
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-char c = (char)va_arg(args, int);
+char c = va_arg(arg, int);
 write(1, &c, 1);
-count++;
+return (1);
 }
-else if (*format == 's')
+
+/**
+ * print_str - Prints a string to the standard output
+ * @arg: A va_list containing the string to print
+ * Return: The number of characters printed
+*/
+int print_str(va_list arg)
 {
-char *s = va_arg(args, char*);
+char *s = va_arg(arg, char *);
 int len = 0;
-while (s[len] != '\0')
+
+if (s == NULL)
+{
+write(1, "(null)", 6);
+return (6);
+}
+while (s[len])
 len++;
 write(1, s, len);
-count += len;
+return (len);
 }
-else if (*format == '%')
+
+/**
+ * print_percent - prints a percent character to the standard output
+ * @arg: A va_list containing no arguments (unused)
+ * Return: The number of characters printed (always 1)
+*/
+int print_percent(va_list arg)
 {
+(void)arg;
 write(1, "%", 1);
-count++;
+return (1);
 }
-}
-else
+
+/**
+ * _printf - Custom printf function that supports %c, %s, and %%
+ * @format: A format string containing zero or more directives
+ * Return: The number of characters printed
+*/
+int _printf(const char *format, ...)
 {
-write(1, format, 1);
-count++;
+va_list arg;
+int i, len = 0;
+va_start(arg, format);
+
+if (format == NULL)
+return (-1);
+
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+write(1, &format[i], 1);
+len++;
+continue;
 }
-format++;
+
+switch (format[++i])
+{
+case 'c':
+len += print_char(arg);
+break;
+
+case 's':
+len += print_str(arg);
+break;
+
+case '%':
+len += print_percent(arg);
+break;
+
+default:
+write(1, "%", 1);
+write(1, &format[i], 1);
+len += 2;
+break;
 }
-va_end(args);
-return (count);
+}
+
+va_end(arg);
+return (len);
 }
