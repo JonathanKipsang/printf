afd0db05664c76cb4c1ddb70dfb875fb28dbb29c
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-03-26 22:12:57 +0300

    pulled, deleted _printf.c file and pushed my code

---
+++ 0-printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - prints a name
+ * @format: string containing the name to print
+ * Return: printed characters
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int count = 0;
+va_start(args, format);
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+count += putchar(va_arg(args, int));
+break;
+case 's':
+count += puts(va_arg(args, char *));
+break;
+case '%':
+count += putchar('%');
+break;
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++
+}
+va_end(args);
+return (count);
+}
---
+++ Tests/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+/*This is the main.h file that contains all prototype functions*/
+int _printf(const char *format, ...);
+#endif
