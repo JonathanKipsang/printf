f537f95bcbdcd0df932993eae360e783eb6de1bc
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-03-27 13:48:24 +0300

    add specifier for d and i - task 1

--- _printf.c
+++ _printf.c
@@ -30,6 +30,32 @@ int _puts(char *s)
 	}
 	return (i);
 }
+
+/**
+* print_number - helper function used to print out an int
+* @n: the number we want to be printed or putcharred
+*
+* Description: so i did binary pretty simple but i got large binary int
+*	instead of string it in an array i wanted a way to print each digit
+* Return: returns the count number! so we can add it to our counter
+*/
+
+int print_number(int n)
+{
+int div;
+int count = 0;
+
+for (div = 1; div <= n; div *= 10)
+;
+while (n > 0)
+{
+div /= 10;
+_putchar(n / div + '0');
+n %= div;
+count++;
+}
+return (count);
+}
 /**
  * handle_specifier - handles the conversion specifier character
  * @specifier: the conversion specifier character
