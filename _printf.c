6262f3154a89bd9a7d3d13fd1698f13dd9e83124
JonathanKipsang <jonathankipchirchir7@gmail.com>
2023-03-27 05:02:59 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -44,89 +44,12 @@ int _printf(const char *format, ...)
 			printed_chars += printed;
 		}
 	}
-<<<<<<< HEAD
 
 	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
 	return (printed_chars);
-=======
-	return (i);
-}
-
-
-/**
- * print_number - Prints an integer
- * @n: The integer to print
- *
- * Return: The number of digits printed
- */
-int print_number(int n)
-{
-int count = 0;
-
-if (n < 0)
-{
-_putchar('-');
-count++;
-n = -n;
-}
-
-if (n / 10)
-{
-count += print_number(n / 10);
-}
-
-_putchar((n % 10) + '0');
-count++;
-
-return (count);
-}
-/**
- * handle_specifier - handles the conversion specifier character
- * @specifier: the conversion specifier character
- * @arg_list: the list of arguments to be printed
- * Return: the number of characters printed
- *
- * Description: Determines which conversion function to call based on the
- * conversion specifier character, and passes the corresponding argument(s)
- * from arg_list to that function. Returns the total number of characters
- * printed.
- */
-int handle_specifier(char specifier, va_list arg_list)
-{
-int count = 0;
-char *str;
-int num;
-
-switch (specifier)
-{
-case 'c':
-count += _putchar(va_arg(arg_list, int));
-break;
-case 's':
-str = va_arg(arg_list, char *);
-if (str == NULL)
-count += _puts("(null)");
-else
-count += _puts(str);
-break;
-case 'd':
-case 'i':
-num = va_arg(arg_list, int);
-count += print_number(num);
-break;
-case '%':
-count += _putchar('%');
-break;
-default:
-count += _putchar('%');
-count += _putchar(specifier);
-break;
-}
-return (count);
->>>>>>> 03b262eee7358e801917af6e41cc201c534e4fe4
 }
 
 /**
